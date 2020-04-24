270447c4b0216d7048603b38985ca53419c90a54
Ryan Nguyen <ryannguyentt@gmail.com>
2020-04-24 18:42:36 +0000

    Lastest site built on successful travis build 9 auto-pushed to github

--- js/section-nav.js
+++ js/section-nav.js
@@ -1,20 +1,22 @@
-window.addEventListener('DOMContentLoaded', () => {
+function highlightNav() {
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          const id = entry.target.getAttribute('id');
+          if (entry.intersectionRatio > 0) {
+              document.querySelector(`.section-nav-container li a[href="#${id}"]`).parentElement.classList.add('active');
+          } else {
+              document.querySelector(`.section-nav-container li a[href="#${id}"]`).parentElement.classList.remove('active');
+          }
+      });
+  });
 
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
-            if (entry.intersectionRatio > 0) {
-                document.querySelector(`.section-nav-container li a[href="#${id}"]`).parentElement.classList.add('active');
-            } else {
-                document.querySelector(`.section-nav-container li a[href="#${id}"]`).parentElement.classList.remove('active');
-            }
-        });
-    });
+  // Track all h2 tags that have an `id` applied
+  document.querySelectorAll('h2[id]').forEach((h2) => {
+      observer.observe(h2);
+  });
+}
 
-    // Track all h2 tags that have an `id` applied
-    document.querySelectorAll('h2[id]').forEach((h2) => {
-        observer.observe(h2);
-    });
-    
-});
+// In case the document is already rendered
+if (document.readyState != 'loading') highlightNav();
+else document.addEventListener('DOMContentLoaded', highlightNav);
 
